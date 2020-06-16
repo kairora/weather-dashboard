878091ec94d2c5cd7ac1179d3f019afeebf6db5d
Brianna Bullock <brianna.bullock16@gmail.com>
2020-06-15 22:51:23 -0500

    fixed the onclick function

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+    
+
+    $("#searchBtn").on("click", function(event) {
     var city = $(".input").val();
+    
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + token;
-    
-    $("#find-movie").on("click", function(event) {
-    
         $.ajax({
             url: queryURL,
             method: "GET",
