5f25dfb3d08353923a854b7976a95f45732b6d59
Brianna Bullock <brianna.bullock16@gmail.com>
2020-06-18 00:29:30 -0500

    display last search top

--- Js/script.js
+++ Js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     var cityArr = [];
     var unOrdList = $("#cities")
     var firstAJAX;
+    var m = moment();
+
 
     $("#searchBtn").on("click", function (event) {
         var city = $(".input").val();
@@ -16,13 +18,13 @@ $(document).ready(function () {
         //         return cityBtnTxt;
         //     }
         // }
-        event.preventDefault(); 
-        
-        
+        event.preventDefault();
+
+
         $.ajax({
             url: queryURL,
             method: "GET",
-            success: function(data){
+            success: function (data) {
                 firstAJAX = data;
             }
         }).then(function (response) {
@@ -32,35 +34,36 @@ $(document).ready(function () {
             var UTC = response.timezone / 60;
             // inputs UTC offset and outputs a date stored in var
             var date = m.utcOffset(UTC).format("M/DD/YYYY");
+            localStorage.setItem("NowDate", date)
             // displays city name + date
             $(".currentcity").text((response.name) + " " + "(" + date + ")");
-            var stringName = JSON.stringify(response.name)
-            localStorage.setItem("NowCity", stringName);
+            var storedName = response.name
+            localStorage.setItem("NowCity", storedName);
             // icon
             $(".weatherIcon").attr("class", response.weather.icon);
-            
+
             // temperature display and set into localStorage
             $("#ctemp").text("Temperature: " + tempConv.toFixed(1) + "°F");
             localStorage.setItem("NowTemp", tempConv.toFixed(1) + "°F");
             // humidity display and set into localStorage
             $("#chumid").text("Humidity: " + response.main.humidity + "%");
-            var stringHum = JSON.stringify(response.main.humidity + "%")
-            localStorage.setItem("NowHum", stringHum);
+            var storedHum = response.main.humidity + "%"
+            localStorage.setItem("NowHum", storedHum);
             // wind speed display and set into localStorage
             $("#cwind").text("Wind Speed: " + response.wind.speed + " MPH");
-            var stringWind = JSON.stringify(response.wind.speed + " MPH")
-            localStorage.setItem("NowHum", stringWind);
-            
-           
+            var storedWind = response.wind.speed + " MPH"
+            localStorage.setItem("NowWind", storedWind);
+
+
             var lat = response.coord.lat;
             var lon = response.coord.lon;
             // console.log(response);
             // console.log("lat: " + lat);
             // console.log("lon: " + lon);
-           
+
             var UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + token + "&lat=" + lat + "&lon=" + lon;
             var uvValue = response.value
-            
+
             $.ajax({
                 url: UVqueryURL,
                 method: "GET",
@@ -68,19 +71,20 @@ $(document).ready(function () {
                 // Creates and inserts UV value with colored background depending on severity level
                 var uvValue = response.value
                 $("#cuv").text("UV Index: ");
-                $("#cuv").append($("<span>", {class: "cSpan"}));
+                $("#cuv").append($("<span>", { class: "cSpan" }));
                 $(".cSpan").text(uvValue);
+                localStorage.setItem("NowUV", uvValue);
 
 
 
                 console.log(uvValue)
-                if (uvValue >= 0 && uvValue <= 2.999 ) {
+                if (uvValue >= 0 && uvValue <= 2.999) {
                     $(".cSpan").attr("id", "low")
-                } else if (uvValue >= 3 && uvValue <= 5.999 ) {
+                } else if (uvValue >= 3 && uvValue <= 5.999) {
                     $(".cSpan").attr("id", "moderate")
-                } else if (uvValue >= 6 && uvValue <= 7.999 ) {
+                } else if (uvValue >= 6 && uvValue <= 7.999) {
                     $(".cSpan").attr("id", "midhigh")
-                } else if (uvValue >= 8 && uvValue <= 10.999 ) {
+                } else if (uvValue >= 8 && uvValue <= 10.999) {
                     $(".cSpan").attr("id", "high")
                 } else if (uvValue >= 11) {
                     $(".cSpan").attr("id", "severe")
@@ -88,7 +92,7 @@ $(document).ready(function () {
                     $(".cSpan").text("UV Index: N/A")
                 }
             });
-            
+
             var stringAJAX = JSON.stringify(firstAJAX)
             // // JSON.parse(firstAJAX);
             // console.log("first AJAX: " + JSON.parse(stringAJAX));
@@ -108,7 +112,7 @@ $(document).ready(function () {
             // $("#cwind").text("Wind Speed: " + firstAJAX.data.wind.speed + " MPH");
         });
 
-        
+
         // $(".currentcity").text((response.name) + " " + "(" + date + ")");
         //     // icon
         //     $(".weatherIcon").attr("class", response.weather.icon);
@@ -118,8 +122,8 @@ $(document).ready(function () {
         //     $("#chumid").text("Humidity: " + response.main.humidity + "%");
         //     // wind speed
         //     $("#cwind").text("Wind Speed: " + response.wind.speed + " MPH");
-        
-        
+
+
 
         // adds cities to array
         cityArr.push(city);
@@ -130,7 +134,7 @@ $(document).ready(function () {
         // turns string into an array
         JSON.parse(value);
         // inserts the city input to an LI element with class and styles
-        unOrdList.append($("<li>", {class: "cityLI"}).text(city));
+        unOrdList.append($("<li>", { class: "cityLI" }).text(city));
         // grabs the last city in the array
         var lastCity = cityArr[cityArr.length - 1];
 
@@ -147,25 +151,47 @@ $(document).ready(function () {
         cityArr = parsedCities;
         var newOrdList = null;
         parsedCities.forEach(city => {
-            newOrdList = unOrdList.append($("<li>", {class: "cityLI"}).text(city));
-            
+            newOrdList = unOrdList.append($("<li>", { class: "cityLI" }).text(city));
+
         });
         $("#cities").append(newOrdList);
     }
-
-    
+    // date +
+    // Display last saved city data
+    // Last City
+    $(".currentcity").text(localStorage.getItem("NowCity") + " " + "(" + localStorage.getItem("NowDate") + ")");
     // var lastTempConv = (firstAJAX.data.main.temp - 273.15) * (9 / 5) + 32;
-    $
-    // // icon
+
+    // icon
     // $(".weatherIcon").attr("class", firstAJAX.data.weather.icon);
-    // // temperature
-    // $("#ctemp").text("Temperature: " + lastTempConv.toFixed(1) + "°F");
+    // temperature
+    $("#ctemp").text("Temperature: " + localStorage.getItem("NowTemp"));
     // // humidity
-    // $("#chumid").text("Humidity: " + firstAJAX.data.main.humidity + "%");
-    // // wind speed
-    // $("#cwind").text("Wind Speed: " + firstAJAX.data.wind.speed + " MPH");
-    
-    
+    $("#chumid").text("Humidity: " + localStorage.getItem("NowHum"));
+    // wind speed
+    $("#cwind").text("Wind Speed: " + localStorage.getItem("NowWind"));
+    // UV 
+    var lastUV = localStorage.getItem("NowUV")
+    $("#cuv").text("UV Index: ");
+    $("#cuv").append($("<span>", { class: "cSpan" }));
+    $(".cSpan").text(lastUV);
+    if (lastUV >= 0 && lastUV <= 2.999) {
+        $(".cSpan").attr("id", "low")
+    } else if (lastUV >= 3 && lastUV <= 5.999) {
+        $(".cSpan").attr("id", "moderate")
+    } else if (lastUV >= 6 && lastUV <= 7.999) {
+        $(".cSpan").attr("id", "midhigh")
+    } else if (lastUV >= 8 && lastUV <= 10.999) {
+        $(".cSpan").attr("id", "high")
+    } else if (lastUV >= 11) {
+        $(".cSpan").attr("id", "severe")
+    } else {
+        $(".cSpan").text("UV Index: N/A")
+    }
+
+
+
+
 
     // grabs the last city in the array
     // var lastCity = parsedCities[parsedCities.length - 1];
